92ec7b78b10d6a05f09d71793b5b78b10644ae5a
Tyler Holewinski <tyler.holewinski@bryx.com>
2022-09-14 19:53:11 -0400

    initial commit

---
+++ popup.js
@@ -0,0 +1,67 @@
+let changeColor = document.getElementById("changeColor");
+
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: makeDarkMode,
+  });
+});
+
+function makeDarkMode() {
+  const RULES = {
+    "div.retool-grid": {
+      backgroundColor: "#533483",
+    },
+    h2: {
+      color: "white",
+    },
+    "div.retool-canvas-container": {
+      backgroundColor: "#16213E",
+    },
+    '.user-select-none > :nth-child(1)': {
+      backgroundColor: '#1B1616'
+    },
+    'rect.bg': {
+      fill: "#1B1616"
+    },
+    '.gtitle': {
+      fill: 'white',
+    },
+    '._retool-container-chart2 .user-select-none > :nth-child(1)': {
+      backgroundColor: '#1B1616'
+    },
+    '._retool-container-chart2 .user-select-none': {
+      fill: 'white'
+    },
+    '._retool-container-chart2 .user-select-none rect.bg': {
+      fill: "#1B1616"
+    },
+    '._retool-container-chart2 .user-select-none .gtitle': {
+      fill: "white"
+    },
+    '._retool-container-chart2 .user-select-none .g-gtitle': {
+      fill: "white"
+    },
+    '#table-table1': {
+      borderWidth: '0.5px'
+    },
+    '#table-table2': {
+      borderWidth: '0.5px'
+    },
+    '.retool-fullscreen-container > :nth-child(2)': {
+      display: 'none'
+    },
+    'main': {
+      backgroundColor: "#16213E",
+    },
+  };
+
+  Object.keys(RULES).forEach((key) => {
+    const newStyling = RULES[key];
+    Object.keys(newStyling).forEach((styleKey) => {
+      document.querySelector(key).style[styleKey] = newStyling[styleKey];
+      console.log(document.querySelector(key));
+    });
+  });
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Bagelbot Dashboard Dark Mode Injector
+
+While on the bagelbot dashboard page, click the extension icon, and then the "Apply Dark Mode" button to inject updated manual styling on the Retool dashboard.
+
+
